35e7e59c915111891a191bb1b8abb38c97211c1b
Foez Ahmed <foez.official@gmail.com>
2026-03-31 17:00:04 +0600

    modified:   hardware/filelist/s1.f new file:   hardware/source/s1_clk_div.sv new file:   hardware/source/s1_dual_edge_register.sv

--- hardware/filelist/s1.f
+++ hardware/filelist/s1.f
@@ -4,5 +4,8 @@
 ${AXI}/src/axi_pkg.sv
 ${S1}/hardware/package/s1_uart_pkg.sv
 
+${S1}/hardware/source/s1_clk_div.sv
+${S1}/hardware/source/s1_dual_edge_register.sv
+
 ${S1}/hardware/source/s1_fifo.sv
 ${S1}/hardware/source/s1_uart_regif.sv
