15e90e83d51cae07a129745fa476258394323609
Md. Mohiuddin Reyad <mreyad30207@gmail.com>
2026-04-08 15:19:06 +0600

    added s1_axi_cvtr and axi_xbar in s1_soc

--- hardware/source/s1_soc.sv
+++ hardware/source/s1_soc.sv
@@ -17,7 +17,22 @@ module s1_soc
   import s1_soc_pkg::std_axil_resp_t;
   import s1_soc_pkg::std_axi_req_t;
   import s1_soc_pkg::std_axi_resp_t;
-(
+  import s1_soc_pkg::snoc_sp_req_t;
+  import s1_soc_pkg::snoc_sp_resp_t;
+  import s1_soc_pkg::snoc_mp_req_t;
+  import s1_soc_pkg::snoc_mp_aw_chan_t;
+  import s1_soc_pkg::snoc_mp_resp_t;
+  import s1_soc_pkg::cnoc_sp_req_t;
+  import s1_soc_pkg::cnoc_sp_resp_t;
+  import s1_soc_pkg::cnoc_mp_req_t;
+  import s1_soc_pkg::cnoc_mp_resp_t;
+  import s1_soc_pkg::pnoc_sp_req_t;
+  import s1_soc_pkg::pnoc_sp_resp_t
+  import s1_soc_pkg::pnoc_mp_req_t;
+  import s1_soc_pkg::pnoc_mp_resp_t;
+  import s1_soc_pkg::snoc_xbar_cfg;
+  import s1_soc_pkg::SnocXbarRule;
+  (
     input logic temp_arst_snoc_ni,
     input logic temp_arst_pnoc_ni,
     input logic temp_clk_snoc_i,
@@ -46,8 +61,15 @@ module s1_soc
 
   std_axil_req_t  asi_asian_req;
   std_axil_resp_t asi_asian_resp;
-  std_axi_req_t   axil_to_axi_req;  // TODO REMOVE
-  std_axi_resp_t  axil_to_axi_resp;  // TODO REMOVE
+  std_axi_req_t   asin_asiw_req;  // TODO REMOVE
+  std_axi_resp_t  asin_asiw_resp;  // TODO REMOVE
+
+  snoc_sp_req_t   asiw_snoc_req;
+  snoc_sp_resp_t  asiw_snoc_resp;
+
+  snoc_mp_req_t   snoc_snocn_req;
+  snoc_mp_resp_t  snoc_snocn_resp;
+
   std_axil_req_t  axi_to_axil_req;  // TODO REMOVE
   std_axil_resp_t axi_to_axil_resp;  // TODO REMOVE
 
@@ -63,7 +85,7 @@ module s1_soc
       .r_chan_t  (std_axil_r_chan_t),
       .axi_req_t (std_axil_req_t),
       .axi_resp_t(std_axil_resp_t)
-  ) apb_slave_in (
+  ) asi (  //apb_slave_in
       .apb_clk_i  (apb_s_clk_i),
       .apb_arst_ni(apb_s_arst_ni),
       .apb_req_i  (apb_s_req_i),
@@ -79,12 +101,62 @@ module s1_soc
       .axil_resp_t(std_axil_resp_t),
       .axi_req_t  (std_axi_req_t),
       .axi_resp_t (std_axi_resp_t)
-  ) apb_slave_in_axi_narrow (
-      .axil_req_i(asi_asian_req),
-      .axil_resp_o(asi_asian_resp),
-      .axi_req_o(axil_to_axi_req),
-      .axi_resp_i(axil_to_axi_resp)
+  ) asin (  // apb_slave_in_axi_narrow
+      .axil_req_i (asi_asian_req  ),
+      .axil_resp_o(asi_asian_resp ),
+      .axi_req_o  (asin_asiw_req  ),
+      .axi_resp_i (asin_asiw_resp )
   );
+
+  s1_axi_cvtr #(
+      .src_req_t (std_axi_req_t),
+      .src_resp_t(std_axi_resp_t),
+      .dst_req_t (std_axi_req_t),
+      .dst_resp_t(std_axi_resp_t),
+      .enable_cdc(0),
+      .faster_src(1)
+  ) asiw (
+      .arst_ni     ( temp_arst_snoc_ni),
+      .src_clk_i   ( temp_clk_snoc_i  ),
+      .src_req_i   ( asin_asiw_req    ),
+      .src_resp_o  ( asin_asiw_resp   ),
+      .dst_clk_i   ( temp_clk_snoc_i  ),
+      .dst_req_o   ( asiw_snoc_req    ),
+      .dst_resp_i  ( asiw_snoc_resp   ),
+      .addr_shift_i( )      // TODO:fill
+  );
+
+  axi_xbar #(
+    .Cfg           ( snoc_xbar_cfg     ),
+    .ATOPs         ( '0                ),
+    .Connectivity  ( '1                ),
+    .slv_aw_chan_t ( snoc_mp_aw_chan_t ),
+    .mst_aw_chan_t ( snoc_sp_aw_chan_t ),
+    .w_chan_t      ( snoc_mp_w_chant_t ),    //TODO: connection
+    .slv_b_chan_t  ( snoc_mp_b_chan_t  ),
+    .mst_b_chan_t  ( snoc_sp_b_chan_t  ),
+    .slv_ar_chan_t ( snoc_mp_ar_chan_t ),
+    .mst_ar_chan_t ( snoc_sp_ar_chan_t ),
+    .slv_r_chan_t  ( snoc_mp_r_chan_t  ),
+    .mst_r_chan_t  ( snoc_sp_r_chant_t ),
+    .slv_req_t     ( snoc_mp_req_t     ),
+    .slv_resp_t    ( snoc_mp_resp_t    ),
+    .mst_req_t     ( snoc_sp_req_t     ),
+    .mst_resp_t    ( snoc_sp_resp_t    ),
+    .rule_t        ( xbar_rule_64_t    )
+) snoc (
+    .clk_i                 (temp_clk_snoc_i   ),
+    .rst_ni                (temp_arst_snoc_ni ),
+    .test_i                ('0                ),
+    .slv_ports_req_i       (asiw_snoc_req     ),  //TODO confusion
+    .slv_ports_resp_o      (asiw_snoc_resp    ),  //TODO confusion
+    .mst_ports_req_o       (snoc_snocn_req    ),  //TODO confusion
+    .mst_ports_resp_i      (snoc_snocn_resp   ),  //TODO confusion
+    .addr_map_i            (SnocXbarRule      ),
+    .en_default_mst_port_i ('1 ),
+    .default_mst_port_i    ('0 )
+);
+
 
   axi_to_axi_lite #(
       .AxiAddrWidth   (AXI_ADDR_WIDTH),
@@ -103,8 +175,8 @@ module s1_soc
       .clk_i(temp_clk_pnoc_i),
       .rst_ni(temp_arst_pnoc_ni),
       .test_i('0),
-      .slv_req_i(axil_to_axi_req),
-      .slv_resp_o(axil_to_axi_resp),
+      .slv_req_i(asin_asiw_req),
+      .slv_resp_o(asin_asiw_resp),
       .mst_req_o(axi_to_axil_req),
       .mst_resp_i(axi_to_axil_resp)
   );
